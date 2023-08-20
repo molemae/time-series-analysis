71526e08e93188dd1f16f1fa6ef9bef2cc9f8220
Moe <moritz.maehrlein@posteo.de>
2023-08-20 15:40:27 +0200

    added autoregression to class_TimeSeries.py

--- class_TimeSeries.py
+++ class_TimeSeries.py
@@ -10,11 +10,11 @@ class TimeSeriesFrame:
         self.model = None
         self.plot_acf = None
         self.plot_pacf = None
-        self.ts_trend()
-        self.ts_seasonality()
-        self.ts_remainder()
+        self.trend()
+        self.seasonality()
+        self.remainder()
         
-    def ts_trend(self):
+    def trend(self):
         self.data['timestep'] = list(range(len(self.data)))
         X = self.data[['timestep']]
         m = LinearRegression()
@@ -22,7 +22,7 @@ class TimeSeriesFrame:
         self.data['trend'] = m.predict(X)
         print('Trend: Linear Regressio', '\nIntercept:', m.intercept_, '\nSlope: ', m.coef_)
 
-    def ts_seasonality(self):
+    def seasonality(self):
         seasonal_dummies = pd.get_dummies(self.data.index.month, prefix='month')
         seasonal_dummies = seasonal_dummies.set_index(self.data.index)
         self.data = pd.concat([self.data, seasonal_dummies], axis=1)
@@ -31,12 +31,79 @@ class TimeSeriesFrame:
         m.fit(X, self.data[self.y_column])
         self.data['seasonal'] = m.predict(X)
 
-    def ts_remainder(self):
+    def remainder(self):
         X = self.data.filter(regex='timestep|month', axis=1)
         m = LinearRegression()
         m.fit(X, self.data[self.y_column])
         self.data['trend_seasonal'] = m.predict(X)
         self.data['remainder'] = self.data[self.y_column] - self.data['trend_seasonal']
+    
+    def autoregression(df,lagcount,rm_na=True):
+        """Creates and visualizes time lagged-input feature(s) of the remainder in given data frame.
+
+        Args:
+            df (pd.DataFrame): Input data frame containing a remainder of a time series
+            lagcount (int): Number of lag features 
+            rm_na (bool, optional): Wether to remove NA values and calculate the autoregression. Defaults to True.
+
+        Returns:
+            _type_: DataFrame containing with lag columns added
+        """
+        
+        # Plotting lag vs remainder:
+        fig, ax = plt.subplots(nrows=3, ncols=lagcount, figsize= ((lagcount*6),12)) 
+        for _lag in range(lagcount):
+            # create column
+            lag_col_name = 'lag' + str(_lag+1)
+            df.loc[:,lag_col_name] = df['remainder'].shift(int(_lag+1))
+            
+            # Drop missing values
+            if rm_na is True:
+                df.dropna(inplace=True)
+
+            # Print correlation matrix
+            if (_lag+1) == lagcount:
+                corr_matrix = df[['tg'] + [col for col in df.columns if col.startswith('lag')]].corr()
+                print('Correlatoin matrix: lag vs remainder:\n', corr_matrix)
+
+            # First plot: lag vs remainder:
+            if lagcount==1:
+                sns.scatterplot(x= lag_col_name, y='remainder', data=df, ax=ax[0])
+            else:
+                sns.scatterplot(x= lag_col_name, y='remainder', data=df, ax=ax[0,_lag])
+
+            # auto regressive model:
+            if rm_na is True:
+                # Select the model's X and y
+                X = df[[lag_col_name]]
+                y = df['remainder']
+
+                # Call and fit the model
+                m = LinearRegression()
+                m.fit(X, y)
+
+                # Create predictions
+                pred_col_name = 'pred_' + lag_col_name
+                df[pred_col_name] = m.predict(X)
+
+                # Second plot: the remainder vs prediction
+                if lagcount==1:
+                    sns.scatterplot(x= pred_col_name, y='remainder', data=df, ax=ax[1])
+                else:    
+                    sns.scatterplot(x= pred_col_name, y='remainder', data=df, ax=ax[1,_lag])
+            
+                # Third plot:
+                # # Is the remainder prediction error smaller than the remainder itself?
+                df_plot = df.iloc[-365:].copy()
+                _ylim = 180
+                if lagcount==1:
+                    df_plot['remainder'].plot(ylim=[-_ylim,_ylim], legend =True,ax=ax[2])
+                    (df_plot['remainder'] - df_plot[pred_col_name]).plot(ylim=[-_ylim,_ylim],ax=ax[2])
+                    ax[2].legend(["Remainder", "Pred_Error"]);
+                else:
+                    df_plot['remainder'].plot(ylim=[-_ylim,_ylim], legend =True,ax=ax[2,_lag])
+                    (df_plot['remainder'] - df_plot[pred_col_name]).plot(ylim=[-_ylim,_ylim],ax=ax[2,_lag],legend=True)
+                    ax[2,_lag].legend(["Remainder", "Pred_Error"],frameon=False);
 
     # def ts_autoregression(self, lagcount=1, rm_na=True):
     #     for _lag in range(lagcount):
